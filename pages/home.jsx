2b9716a6590f8ad43c63651e69f4b3fb13889fad
KaLua30000 <beige30000@gmail.com>
2023-03-12 15:20:46 +0800

    3/12

--- pages/home.jsx
+++ pages/home.jsx
@@ -4,10 +4,10 @@ import Footer from "../component/footer"
 
 function Home() {
     return (
-        <div className="container main-layout">
-            <Header className="layout-header" />
-            <ImageList className="layout-content" />
-            <Footer className="layout-footer" />
+        <div>
+            <Header />
+            <ImageList />
+            <Footer />
         </div>
     );
 }
